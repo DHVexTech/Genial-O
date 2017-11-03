dbf31cadd1ba2ae8d47bf7593c8f749d617e63ad
VexTech <herve@intechinfo.fr>
2017-11-03 10:27:11 +0100

    Add server for the robot and change to .Net framework 4.3

---
+++ Genial-O/Genial-O/HttpServer/StringExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Genial_O.HttpServer
+{
+    internal static class StringExtensions
+    {
+        public static bool EndsWith(this string s, string value)
+        {
+            return s.IndexOf(value) == s.Length - value.Length;
+        }
+
+        public static bool StartsWith(this string s, string value)
+        {
+            return s.IndexOf(value) == 0;
+        }
+
+        public static bool Contains(this string s, string value)
+        {
+            return s.IndexOf(value) >= 0;
+        }
+    }
+
+}
