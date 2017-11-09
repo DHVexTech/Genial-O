2ebf0d203c7637628d9df25ae61e1582556bbd01
Karotteman <richer@intechinfo.fr>
2017-11-09 10:13:40 +0100

    Initialize MapController and rename MoveController to RobotController

---
+++ GenialO_backend/src/Génialo_WebApp/Controllers/MapController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GenialO_WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class MapController : Controller
+    {
+        readonly HttpClient _httpClient;
+        string map;
+
+        public MapController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<string> MappingAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("map/mapping");
+
+            if (response.IsSuccessStatusCode)
+            {
+                map = await response.Content.ReadAsStringAsync();
+            }
+            return map;
+        }
+
+        [HttpGet]
+        public async Task<string> GiveAllAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("map/giveAll");
+
+            if (response.IsSuccessStatusCode)
+            {
+                map = await response.Content.ReadAsStringAsync();
+            }
+            return map;
+        }
+
+        [HttpGet]
+        public async Task<string> ChooseAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("map/choose");
+
+            if (response.IsSuccessStatusCode)
+            {
+                map = await response.Content.ReadAsStringAsync();
+            }
+            return map;
+        }
+
+        [HttpGet]
+        public async Task<string> DeleteAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("map/delete");
+
+            if (response.IsSuccessStatusCode)
+            {
+                map = await response.Content.ReadAsStringAsync();
+            }
+            return map;
+        }
+
+        [HttpGet]
+        public async Task<string> PointAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("map/point");
+
+            if (response.IsSuccessStatusCode)
+            {
+                map = await response.Content.ReadAsStringAsync();
+            }
+            return map;
+        }
+
+        [HttpGet]
+        public async Task<string> GoToAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("map/goTo");
+
+            if (response.IsSuccessStatusCode)
+            {
+                map = await response.Content.ReadAsStringAsync();
+            }
+            return map;
+        }
+    }
+}
---
+++ GenialO_backend/src/Génialo_WebApp/Controllers/RobotController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GenialO_WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class RobotController : Controller
+    {
+        readonly HttpClient _httpClient;
+        string move;
+
+        public RobotController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<string> ConnnexionAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("robot/connexion");
+
+            if (response.IsSuccessStatusCode)
+            {
+                move = await response.Content.ReadAsStringAsync();
+            }
+            return move;
+        }
+
+        [HttpGet]
+        public async Task<string> EngineAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("robot/engine");
+
+            if (response.IsSuccessStatusCode)
+            {
+                move = await response.Content.ReadAsStringAsync();
+            }
+            return move;
+        }
+
+        [HttpGet]
+        public async Task<string> SynchronizeAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("robot/synchronize");
+
+            if (response.IsSuccessStatusCode)
+            {
+                move = await response.Content.ReadAsStringAsync();
+            }
+            return move;
+        }
+
+        [HttpGet]
+        public async Task<string> RightAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("robot/right");
+
+            if (response.IsSuccessStatusCode)
+            {
+                move = await response.Content.ReadAsStringAsync();
+            }
+            return move;
+        }
+
+        public async Task<string> LeftAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("robot/left");
+
+            if (response.IsSuccessStatusCode)
+            {
+                move = await response.Content.ReadAsStringAsync();
+            }
+            return move;
+        }
+
+        public async Task<string> FrontAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("robot/front");
+
+            if (response.IsSuccessStatusCode)
+            {
+                move = await response.Content.ReadAsStringAsync();
+            }
+            return move;
+        }
+
+        public async Task<string> BackAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("robot/back");
+
+            if (response.IsSuccessStatusCode)
+            {
+                move = await response.Content.ReadAsStringAsync();
+            }
+            return move;
+        }
+    }
+}
